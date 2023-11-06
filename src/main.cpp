d281926fe002e6db594de1282a3493d39fb12aad
Haruki Ota <ota-haruki@uec.ac.jp>
2023-11-06 16:49:43 +0900

    Remove comments to improve readability

--- src/main.cpp
+++ src/main.cpp
@@ -26,17 +26,9 @@ static NimBLEAdvertisedDevice* advDevice;
 static bool doConnect = false;
 static uint32_t scanTime = 0; /** 0 = scan forever */
 
-/**  None of these are required as they will be handled by the library with defaults. **
- **                       Remove as you see fit for your needs                        */
 class ClientCallbacks : public NimBLEClientCallbacks {
   void onConnect(NimBLEClient* pClient) {
     Serial.println("Connected");
-    /** After connection we should change the parameters if we don't need fast response times.
-     *  These settings are 150ms interval, 0 latency, 450ms timout.
-     *  Timeout should be a multiple of the interval, minimum is 100ms.
-     *  I find a multiple of 3-5 * the interval works best for quick response/reconnect.
-     *  Min interval: 120 * 1.25ms = 150, Max interval: 120 * 1.25ms = 150, 0 latency, 60 * 10ms = 600ms timeout
-     */
     pClient->updateConnParams(120, 120, 0, 60);
   };
 
@@ -46,69 +38,49 @@ class ClientCallbacks : public NimBLEClientCallbacks {
     NimBLEDevice::getScan()->start(scanTime, scanEndedCB);
   };
 
-  /** Called when the peripheral requests a change to the connection parameters.
-   *  Return true to accept and apply them or false to reject and keep
-   *  the currently used parameters. Default will return true.
-   */
   bool onConnParamsUpdateRequest(NimBLEClient* pClient, const ble_gap_upd_params* params) {
-    // Failing to accepts parameters may result in the remote device
-    // disconnecting.
     return true;
   };
 
-  /********************* Security handled here **********************
-   ****** Note: these are the same return values as defaults ********/
   uint32_t onPassKeyRequest() {
     Serial.println("Client Passkey Request");
-    /** return the passkey to send to the server */
     return 123456;
   };
 
   bool onConfirmPIN(uint32_t pass_key) {
     Serial.print("The passkey YES/NO number: ");
     Serial.println(pass_key);
-    /** Return false if passkeys don't match. */
     return true;
   };
 
-  /** Pairing process complete, we can check the results in ble_gap_conn_desc */
   void onAuthenticationComplete(ble_gap_conn_desc* desc) {
     if (!desc->sec_state.encrypted) {
       Serial.println("Encrypt connection failed - disconnecting");
-      /** Find the client with the connection handle provided in desc */
       NimBLEDevice::getClientByID(desc->conn_handle)->disconnect();
       return;
     }
   };
 };
 
-/** Define a class to handle the callbacks when advertisments are received */
 class AdvertisedDeviceCallbacks : public NimBLEAdvertisedDeviceCallbacks {
   void onResult(NimBLEAdvertisedDevice* advertisedDevice) {
-    //    if ((advertisedDevice->getAdvType() == BLE_HCI_ADV_TYPE_ADV_DIRECT_IND_HD)
-    //        || (advertisedDevice->getAdvType() == BLE_HCI_ADV_TYPE_ADV_DIRECT_IND_LD)
-    //        || (advertisedDevice->haveServiceUUID() && advertisedDevice->isAdvertisingService(NimBLEUUID(HID_SERVICE))))
-    if (advertisedDevice->haveServiceUUID() && advertisedDevice->isAdvertisingService(NimBLEUUID(HID_SERVICE))) {
+    if ((advertisedDevice->getAdvType() == BLE_HCI_ADV_TYPE_ADV_DIRECT_IND_HD) ||
+        (advertisedDevice->getAdvType() == BLE_HCI_ADV_TYPE_ADV_DIRECT_IND_LD) ||
+        (advertisedDevice->haveServiceUUID() && advertisedDevice->isAdvertisingService(NimBLEUUID(HID_SERVICE)))) {
       Serial.printf("onResult: AdvType= %d\r\n", advertisedDevice->getAdvType());
       Serial.print("Advertised HID Device found: ");
       Serial.println(advertisedDevice->toString().c_str());
 
-      /** stop scan before connecting */
       NimBLEDevice::getScan()->stop();
-      /** Save the device reference in a global for the client to use*/
       advDevice = advertisedDevice;
-      /** Ready to connect now */
       doConnect = true;
     }
   };
 };
 
-/** Notification / Indication receiving handler callback */
-// Notification from 4c:75:25:xx:yy:zz: Service = 0x1812, Characteristic = 0x2a4d, Value = 1,0,0,0,0,
 void notifyCB(NimBLERemoteCharacteristic* pRemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) {
   std::string str = (isNotify == true) ? "Notification" : "Indication";
   str += " from ";
-  /** NimBLEAddress and NimBLEUUID have std::string operators */
   str += std::string(pRemoteCharacteristic->getRemoteService()->getClient()->getPeerAddress());
   str += ": Service = " + std::string(pRemoteCharacteristic->getRemoteService()->getUUID());
   str += ", Characteristic = " + std::string(pRemoteCharacteristic->getUUID());
@@ -121,85 +93,19 @@ void notifyCB(NimBLERemoteCharacteristic* pRemoteCharacteristic, uint8_t* pData,
     Serial.print(',');
   }
   Serial.print(' ');
-  switch (length) {
-    uint16_t appearance;
-    case 5:
-      // https://github.com/wakwak-koba/ESP32-NimBLE-Mouse
-      // returns 5 bytes per HID report
-      Serial.printf("buttons: %02x, x: %d, y: %d, wheel: %d hwheel: %d", pData[0], (int8_t)pData[1], (int8_t)pData[2], (int8_t)pData[3],
-                    (int8_t)pData[4]);
-      break;
-    case 6:
-      // BLE Trackball Mouse from Amazon returns 6 bytes per HID report
-      typedef struct __attribute__((__packed__)) trackball_mouse {
-        uint8_t buttons;
-        int16_t x;
-        int16_t y;
-        int8_t wheel;
-      } trackball_mouse_t;
-      {
-        trackball_mouse_t* my_mouse;
-        my_mouse = (trackball_mouse_t*)pData;
-        Serial.printf("buttons: %02x, x: %d, y: %d, wheel: %d", my_mouse->buttons, my_mouse->x, my_mouse->y, my_mouse->wheel);
-      }
-      break;
-    case 9:
-      appearance = advDevice->getAppearance();
-      if (appearance == APPEARANCE_HID_MOUSE) {
-        // MS BLE Mouse returns 9 bytes per HID report
-        typedef struct __attribute__((__packed__)) ms_mouse {
-          uint8_t buttons;
-          int16_t x;
-          int16_t y;
-          int16_t wheel;
-          int16_t pan;
-        } ms_mouse_t;
-        {
-          ms_mouse_t* my_mouse;
-          my_mouse = (ms_mouse_t*)pData;
-          Serial.printf("buttons: %02x, x: %d, y: %d, wheel: %d, pan: %d", my_mouse->buttons, my_mouse->x, my_mouse->y, my_mouse->wheel,
-                        my_mouse->pan);
-        }
-      } else if (appearance == APPEARANCE_HID_GAMEPAD) {
-        typedef struct __attribute__((__packed__)) {
-          uint8_t x;
-          uint8_t y;
-          uint8_t z;
-          uint8_t rz;
-          uint8_t brake;
-          uint8_t accel;
-          uint8_t hat;
-          uint16_t buttons;
-        } joystick_t;
-        {
-          joystick_t* my_joystick;
-          my_joystick = (joystick_t*)pData;
-          Serial.printf("buttons: %02x, x: %d, y: %d", my_joystick->buttons, my_joystick->x, my_joystick->y);
-        }
-      }
-      break;
-  }
   Serial.println();
 }
 
-/** Callback to process the results of the last scan or restart it */
 void scanEndedCB(NimBLEScanResults results) { Serial.println("Scan Ended"); }
 
-/** Create a single global instance of the callback class to be used by all clients */
 static ClientCallbacks clientCB;
 
-/** Handles the provisioning of clients and connects / interfaces with the server */
 bool connectToServer() {
   NimBLEClient* pClient = nullptr;
   bool reconnected = false;
 
   Serial.printf("Client List Size: %d\r\n", NimBLEDevice::getClientListSize());
-  /** Check if we have a client we should reuse first **/
   if (NimBLEDevice::getClientListSize()) {
-    /** Special case when we already know this device, we send false as the
-     *  second argument in connect() to prevent refreshing the service database.
-     *  This saves considerable time and power.
-     */
     pClient = NimBLEDevice::getClientByPeerAddress(advDevice->getAddress());
     if (pClient) {
       Serial.println("Reconnect try");
@@ -210,15 +116,11 @@ bool connectToServer() {
       Serial.println("Reconnected client");
       reconnected = true;
     }
-    /** We don't already have a client that knows this device,
-     *  we will check for a client that is disconnected that we can use.
-     */
     else {
       pClient = NimBLEDevice::getDisconnectedClient();
     }
   }
 
-  /** No client to reuse? Create a new one. */
   if (!pClient) {
     if (NimBLEDevice::getClientListSize() >= NIMBLE_MAX_CONNECTIONS) {
       Serial.println("Max clients reached - no more connections available");
@@ -230,17 +132,10 @@ bool connectToServer() {
     Serial.println("New client created");
 
     pClient->setClientCallbacks(&clientCB, false);
-    /** Set initial connection parameters: These settings are 15ms interval, 0 latency, 120ms timout.
-     *  These settings are safe for 3 clients to connect reliably, can go faster if you have less
-     *  connections. Timeout should be a multiple of the interval, minimum is 100ms.
-     *  Min interval: 12 * 1.25ms = 15, Max interval: 12 * 1.25ms = 15, 0 latency, 51 * 10ms = 510ms timeout
-     */
     pClient->setConnectionParams(12, 12, 0, 51);
-    /** Set how long we are willing to wait for the connection to complete (seconds), default is 30. */
     pClient->setConnectTimeout(5);
 
     if (!pClient->connect(advDevice)) {
-      /** Created a client but failed to connect, don't need to keep it as it has no data */
       NimBLEDevice::deleteClient(pClient);
       Serial.println("Failed to connect, deleted client");
       return false;
@@ -259,20 +154,15 @@ bool connectToServer() {
   Serial.print("RSSI: ");
   Serial.println(pClient->getRssi());
 
-  /** Now we can read/write/subscribe the charateristics of the services we are interested in */
   NimBLERemoteService* pSvc = nullptr;
   NimBLERemoteCharacteristic* pChr = nullptr;
   NimBLERemoteDescriptor* pDsc = nullptr;
 
   pSvc = pClient->getService(HID_SERVICE);
-  if (pSvc) { /** make sure it's not null */
+  if (pSvc) {
     if (!reconnected) {
-      // This returns the HID report descriptor like this
-      // HID_REPORT_MAP 0x2a4b Value: 5,1,9,2,A1,1,9,1,A1,0,5,9,19,1,29,5,15,0,25,1,75,1,
-      // Copy and paste the value digits to http://eleccelerator.com/usbdescreqparser/
-      // to see the decoded report descriptor.
       pChr = pSvc->getCharacteristic(HID_REPORT_MAP);
-      if (pChr) { /** make sure it's not null */
+      if (pChr) {
         Serial.print("HID_REPORT_MAP ");
         if (pChr->canRead()) {
           std::string value = pChr->readValue();
@@ -290,10 +180,6 @@ bool connectToServer() {
       }
     }
 
-    // Subscribe to characteristics HID_REPORT_DATA.
-    // One real device reports 2 with the same UUID but
-    // different handles. Using getCharacteristic() results
-    // in subscribing to only one.
     std::vector<NimBLERemoteCharacteristic*>* charvector;
     charvector = pSvc->getCharacteristics(true);
     for (auto& it : *charvector) {
@@ -318,58 +204,29 @@ void setup() {
   Serial.begin(115200);
 
   Serial.println("Starting NimBLE HID Client");
-  /** Initialize NimBLE, no device name spcified as we are not advertising */
-  NimBLEDevice::init("");
+  NimBLEDevice::init("ps2ble");
 
-  /** Set the IO capabilities of the device, each option will trigger a different pairing method.
-   *  BLE_HS_IO_KEYBOARD_ONLY    - Passkey pairing
-   *  BLE_HS_IO_DISPLAY_YESNO   - Numeric comparison pairing
-   *  BLE_HS_IO_NO_INPUT_OUTPUT - DEFAULT setting - just works pairing
-   */
-  // NimBLEDevice::setSecurityIOCap(BLE_HS_IO_KEYBOARD_ONLY); // use passkey
-  // NimBLEDevice::setSecurityIOCap(BLE_HS_IO_DISPLAY_YESNO); //use numeric comparison
-
-  /** 2 different ways to set security - both calls achieve the same result.
-   *  no bonding, no man in the middle protection, secure connections.
-   *
-   *  These are the default values, only shown here for demonstration.
-   */
+  NimBLEDevice::setSecurityIOCap(BLE_HS_IO_NO_INPUT_OUTPUT);
   NimBLEDevice::setSecurityAuth(true, true, true);
-  // NimBLEDevice::setSecurityAuth(/*BLE_SM_PAIR_AUTHREQ_BOND | BLE_SM_PAIR_AUTHREQ_MITM |*/ BLE_SM_PAIR_AUTHREQ_SC);
 
-  /** Optional: set the transmit power, default is 3db */
-  NimBLEDevice::setPower(ESP_PWR_LVL_P9); /** +9db */
+  NimBLEDevice::setPower(ESP_PWR_LVL_P9);
 
-  /** Optional: set any devices you don't want to get advertisments from */
-  // NimBLEDevice::addIgnored(NimBLEAddress ("aa:bb:cc:dd:ee:ff"));
-
-  /** create new scan */
   NimBLEScan* pScan = NimBLEDevice::getScan();
 
-  /** create a callback that gets called when advertisers are found */
   pScan->setAdvertisedDeviceCallbacks(new AdvertisedDeviceCallbacks());
 
-  /** Set scan interval (how often) and window (how long) in milliseconds */
   pScan->setInterval(45);
   pScan->setWindow(15);
 
-  /** Active scan will gather scan response data from advertisers
-   *  but will use more energy from both devices
-   */
   pScan->setActiveScan(true);
-  /** Start scanning for advertisers for the scan time specified (in seconds) 0 = forever
-   *  Optional callback for when scanning stops.
-   */
   pScan->start(scanTime, scanEndedCB);
 }
 
 void loop() {
-  /** Loop here until we find a device we want to connect to */
   if (!doConnect) return;
 
   doConnect = false;
 
-  /** Found a device we want to connect to, do it now */
   if (connectToServer()) {
     Serial.println("Success! we should now be getting notifications!");
   } else {
